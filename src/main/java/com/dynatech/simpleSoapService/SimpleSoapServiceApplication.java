22e5fc414368f1f48faf9f010ca66ee38d318eb1
lresutik <lresutik@gmail.com>
2023-04-26 10:27:00 +0200

    Init commit

---
+++ src/main/java/com/dynatech/simpleSoapService/SimpleSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dynatech.simpleSoapService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynatech/simpleSoapService/SimpleSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynatech.simpleSoapService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
